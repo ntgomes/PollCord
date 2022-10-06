8101996014cde14f038873787a8f95abcbe5ee0d
abhimanyubellam <bellamabhimanyu@gmail.com>
2022-10-05 20:59:18 -0400

    clean up as per prettier

--- backend/setup/setup.js
+++ backend/setup/setup.js
@@ -1,48 +1,44 @@
-require('dotenv').config()
+require("dotenv").config();
 
-const postgresql= require(  "../postgresql");
-
-
+const postgresql = require("../postgresql");
 
 // Create and Mock up, to clean up nicely and place in functions
 var connection = postgresql();
- // Create
-  connection.query(
-    `
+// Create
+connection
+    .query(
+        `
     CREATE TABLE IF NOT EXISTS polls (
         pollId SERIAL PRIMARY KEY,
         guildId varchar NOT NULL,
         pollName varchar NOT NULL
     );
     `
-).then((res)=>{
-    connection.query(
-        `
+    )
+    .then((res) => {
+        connection
+            .query(
+                `
         CREATE TABLE IF NOT EXISTS questions (
             questionId SERIAL PRIMARY KEY,
             pollId int REFERENCES polls(pollId), 
             questionText varchar NOT NULL
         );
         `
-    ).then(res1=>{
-        connection.query(
-            `
+            )
+            .then((res1) => {
+                connection
+                    .query(
+                        `
             CREATE TABLE IF NOT EXISTS options (
                 questionId int REFERENCES questions(questionId),
                 optionText varchar,
                 count int NOT NULL
             );
             `
-        ).then(res2=>{
-            console.log("DB creation scucesfull");
-        });
+                    )
+                    .then((res2) => {
+                        console.log("DB creation scucesfull");
+                    });
+            });
     });
-    
-});
-
- 
-
-
-
-
-
